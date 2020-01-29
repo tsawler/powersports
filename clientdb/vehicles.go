92649b840476958d3a5ff1878730faf814d5e70f
Trevor Sawler <trevor.sawler@gmail.com>
2020-01-29 15:18:29 -0400

    In progress

--- clientdb/vehicles.go
+++ clientdb/vehicles.go
@@ -2,6 +2,7 @@ package clientdb
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/tsawler/goblender/client/clienthandlers/clientmodels"
 )
 
@@ -11,6 +12,93 @@ type VehicleModel struct {
 
 func (m *VehicleModel) GetAllMotorcycles() ([]clientmodels.Vehicle, error) {
 	var v []clientmodels.Vehicle
+
+	query := `
+		select 
+		       id, 
+		       stock_no, 
+		       coalesce(cost, 0),
+		       vin, 
+		       coalesce(odometer, 0),
+		       coalesce(year, 0),
+		       coalesce(trim, ''),
+		       vehicle_type,
+		       coalesce(body, ''),
+		       coalesce(seating_capacity,''),
+		       coalesce(drive_train,''),
+		       coalesce(engine,''),
+		       coalesce(exterior_color,''),
+		       coalesce(interior_color,''),
+		       coalesce(transmission,''),
+		       coalesce(options,''),
+		       coalesce(model_number, ''),
+		       coalesce(total_msr,0.0),
+		       v.status,
+		       coalesce(description, ''),
+		       vehicle_makes_id,
+		       vehicle_models_id,
+		       hand_picked,
+		       used,
+		       coalesce(price_for_display,''),
+		       created_at,
+		       updated_at
+		from 
+		     vehicles v 
+		where
+			vehicle_type = 7
+			and status = 1
+
+		order by year desc
+		       
+	`
+	rows, err := m.DB.Query(query)
+
+	if err == sql.ErrNoRows {
+		// do nothing
+	} else if err != nil {
+		fmt.Println(err)
+		return v, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		c := &clientmodels.Vehicle{}
+		err = rows.Scan(
+			&c.ID,
+			&c.StockNo,
+			&c.Cost,
+			&c.Vin,
+			&c.Odometer,
+			&c.Year,
+			&c.Trim,
+			&c.VehicleType,
+			&c.Body,
+			&c.SeatingCapacity,
+			&c.DriveTrain,
+			&c.Engine,
+			&c.ExteriorColour,
+			&c.InteriorColour,
+			&c.Transmission,
+			&c.Options,
+			&c.ModelNumber,
+			&c.TotalMSR,
+			&c.Status,
+			&c.Description,
+			&c.VehicleMakesID,
+			&c.VehicleModelsID,
+			&c.HandPicked,
+			&c.Used,
+			&c.PriceForDisplay,
+			&c.CreatedAt,
+			&c.UpdatedAt,
+		)
+		if err != nil {
+			fmt.Println(err)
+			return v, err
+		}
+		current := *c
+		v = append(v, current)
+	}
 
 	return v, nil
 }
