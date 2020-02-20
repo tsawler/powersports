05d0765303d266bac39038be66f033544a1dcb8d
Trevor Sawler <trevor.sawler@gmail.com>
2020-02-20 18:19:24 -0400

    Update URL

--- client-routes.go
+++ client-routes.go
@@ -59,7 +59,7 @@ func ClientRoutes(mux *pat.PatternServeMux, standardMiddleWare, dynamicMiddlewar
 		http.Redirect(w, r, "/motorcyclesforsale/fredericton", http.StatusMovedPermanently)
 	}))
 	mux.Get("/motorcyclesforsale/fredericton", standardMiddleWare.ThenFunc(GetAllMotorcycles))
-	mux.Get("motorcyclesforsale/fredericton/:pageIndex", standardMiddleWare.ThenFunc(GetAllMotorcycles))
+	mux.Get("/motorcyclesforsale/fredericton/:pageIndex", standardMiddleWare.ThenFunc(GetAllMotorcycles))
 
 	// atvs - brute force
 	mux.Get("/atv-brute-force", standardMiddleWare.ThenFunc(func(w http.ResponseWriter, r *http.Request) {
